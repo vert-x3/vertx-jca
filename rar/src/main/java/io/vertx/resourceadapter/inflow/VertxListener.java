7f7351f89ad0e703f5c0bc215573634fc1dbee18
Weston M. Price <wprice@redhat.com>
2014-11-08 20:20:01 -0500

    Vertx JCA   *WIP   -Refactored entire project to be inline with Vertx conventions   -Refactored JEE example for Maven, Vertx etc   -Fixed package names, formatting etc   -Still working on classloading issues in anticipation of moving   project to JCA top level domain

---
+++ rar/src/main/java/io/vertx/resourceadapter/inflow/VertxListener.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package io.vertx.resourceadapter.inflow;
+
+import io.vertx.core.eventbus.Message;
+
+/**
+ *
+ * MDB message listener interface for the Vert.x platform.
+ *
+ * @author Lin Gao <lgao@redhat.com>
+ *
+ */
+public interface VertxListener {
+
+  /**
+   * On Vertx Message.
+   *
+   * @param message
+   *          the message sent from vertx platform.
+   */
+  <T> void onMessage(Message<T> message);
+
+}

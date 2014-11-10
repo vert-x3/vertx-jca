8bc384c6ed0aefd002c16c8c71ddefab6335e12b
Weston M. Price <wprice@redhat.com>
2014-11-10 01:39:37 -0500

    Vert.x JCA   *Added EJB3 deployment descriptor for Wildfly   *Externalized property reference and replacement   *Changed link in send.jsp to new JCA home   *Format, cleanup etc

--- examples/ejb/src/main/java/io/vertx/resourceadapter/examples/mdb/VertxMonitor.java
+++ examples/ejb/src/main/java/io/vertx/resourceadapter/examples/mdb/VertxMonitor.java
@@ -14,9 +14,6 @@ import io.vertx.resourceadapter.inflow.VertxListener;
  * Message-Driven Bean implementation class for: VertxMonitor
  */
 @MessageDriven(name = "VertxMonitor", messageListenerInterface = VertxListener.class, activationConfig = { @ActivationConfigProperty(propertyName = "address", propertyValue = "inbound-address"), })
-//TODO replace this, note we can use the Maven Template plugin because of the delimiter nonsense...very frustrating
-//Need to replace it with the AntRUn plugin
-@ResourceAdapter("vertx-jca-examples-app.ear#vertx-jca-adapter-3.0.0-SNAPSHOT.rar")
 public class VertxMonitor implements VertxListener {
 
   private Logger logger = Logger.getLogger(VertxMonitor.class.getName());
@@ -33,10 +30,10 @@ public class VertxMonitor implements VertxListener {
 
     logger.info("Get a message from Vert.x: " + message.toString());
     T body = message.body();
-    
+
     if (body != null) {
       logger.info("Body of the message: " + body.toString());
-      
+
       if (message.replyAddress() != null) {
         message.reply("Hi, Got your message: " + body.toString());
       }
